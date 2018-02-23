c5c1c717cc374dc221a34e589c5f3a1d81602e11
Jang Insu(word) <isjang98@gmail.com>
2018-02-24 00:51:44 +0900

    Activity 추가
    - SavedListActivity 추가

---
+++ app/src/main/java/kr/co/isdev/simplecounter_android/SavedListActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.isdev.simplecounter_android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SavedListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved_list)
+    }
+}
